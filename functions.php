30ca2d49ff6d87dc34f89ee7fba85d6b4a0395f5
snwelton <snwelton@email.neit.edu>
2015-02-25 20:12:05 -0500

    updated

--- functions.php
+++ functions.php
@@ -1,79 +1,5 @@
 <?php
-    function topTenScoresGame1(){
-        // php code for retrieving data from the Scores table
-        $userID = filter_input(INPUT_POST,'userID');
-        $gameID = filter_input(INPUT_POST,'gameID');
-        $score = filter_input(INPUT_POST,'score');
-			$error_message = '';    
-        $i = 0;
-        $db = new PDO("mysql:host=localhost;dbname=capstonegames", "root", "");
-        $dbs = $db->prepare('SELECT Scores.userID, Scores.gameID, Scores.score, Users.userName FROM Scores INNER JOIN Users ON Scores.userID = Users.userID WHERE GameID =1 ORDER BY score DESC LIMIT 10'); 
-
-        if ( $dbs->execute() && $dbs->rowCount() > 0 ) {
-            $scores = $dbs->fetchAll(PDO::FETCH_ASSOC); 
-			?><ul> Top 10 Scores:</br><?php
-			foreach ($scores as $value) {
-                            $i++;
-                            echo $i;
-			?> Score: <?php echo '<td>', $value['score'],'</td>';?> 
-			User Name: <?php echo '<td>', $value['userName'],'</td>';?></br><?php
-			echo '</tr>';
-				}
-			}
-                        
-    }
-    
-    
-    function topTenScoresGame2(){
-        // php code for retrieving data from the Scores table
-        $userID = filter_input(INPUT_POST,'userID');
-        $gameID = filter_input(INPUT_POST,'gameID');
-        $score = filter_input(INPUT_POST,'score');
-			$error_message = '';    
-        $i = 0;
-        $db = new PDO("mysql:host=localhost;dbname=capstonegames", "root", "");
-        $dbs = $db->prepare('SELECT Scores.userID, Scores.gameID, Scores.score, Users.userName FROM Scores INNER JOIN Users ON Scores.userID = Users.userID WHERE GameID =2 ORDER BY score DESC LIMIT 10'); 
-
-        if ( $dbs->execute() && $dbs->rowCount() > 0 ) {
-            $scores = $dbs->fetchAll(PDO::FETCH_ASSOC); 
-			?><ul> Top 10 Scores:</br><?php
-			foreach ($scores as $value) {
-                            $i++;
-                            echo $i;
-			?> Score: <?php echo '<td>', $value['score'],'</td>';?> 
-			User Name: <?php echo '<td>', $value['userName'],'</td>';?></br><?php
-			echo '</tr>';
-				}
-			}
-                        
-    }
-    
-    
-    function topTenScoresGame3(){
-        // php code for retrieving data from the Scores table
-        $userID = filter_input(INPUT_POST,'userID');
-        $gameID = filter_input(INPUT_POST,'gameID');
-        $score = filter_input(INPUT_POST,'score');
-			$error_message = '';    
-        $i = 0;
-        $db = new PDO("mysql:host=localhost;dbname=capstonegames", "root", "");
-        $dbs = $db->prepare('SELECT Scores.userID, Scores.gameID, Scores.score, Users.userName FROM Scores INNER JOIN Users ON Scores.userID = Users.userID WHERE GameID =3 ORDER BY score DESC LIMIT 10'); 
-
-        if ( $dbs->execute() && $dbs->rowCount() > 0 ) {
-            $scores = $dbs->fetchAll(PDO::FETCH_ASSOC); 
-			?><ul> Top 10 Scores:</br><?php
-			foreach ($scores as $value) {
-                            $i++;
-                            echo $i;
-			?> Score: <?php echo '<td>', $value['score'],'</td>';?> 
-			User Name: <?php echo '<td>', $value['userName'],'</td>';?></br><?php
-			echo '</tr>';
-				}
-			}
-                        
-    }
-    
-    
+ 
     function insertScore()
     {    
         //php code for inserting into scores table
@@ -99,9 +25,171 @@
     
     }
     
+
+    function topTenScoresGame1(){
+        // php code for retrieving data from the Scores table
+        $userID = filter_input(INPUT_POST,'userID');
+        $gameID = filter_input(INPUT_POST,'gameID');
+        $score = filter_input(INPUT_POST,'score');
+	$error_message = '';    
+        $i = 0;
+        $db = new PDO("mysql:host=localhost;dbname=capstonegames", "root", "");
+        $dbs = $db->prepare('SELECT Scores.userID, Scores.gameID, Scores.score, Users.userName FROM Scores INNER JOIN Users ON Scores.userID = Users.userID WHERE GameID =1 ORDER BY score DESC LIMIT 10'); 
+
+        if ( $dbs->execute() && $dbs->rowCount() > 0 ) {
+            $scores = $dbs->fetchAll(PDO::FETCH_ASSOC); 
+			?><ul> Top 10 Scores:</br><?php
+			foreach ($scores as $value) {
+                            $i++;
+                            echo $i;
+			?> Score: <?php echo '<td>', $value['score'],'</td>';?> 
+			User Name: <?php echo '<td>', $value['userName'],'</td>';?></br><?php
+			echo '</tr>';
+				}
+			}                        
+    }
     
+    
+    function topTenScoresGame2(){
+        // php code for retrieving data from the Scores table
+        $userID = filter_input(INPUT_POST,'userID');
+        $gameID = filter_input(INPUT_POST,'gameID');
+        $score = filter_input(INPUT_POST,'score');
+	$error_message = '';    
+        $i = 0;
+        $db = new PDO("mysql:host=localhost;dbname=capstonegames", "root", "");
+        $dbs = $db->prepare('SELECT Scores.userID, Scores.gameID, Scores.score, Users.userName FROM Scores INNER JOIN Users ON Scores.userID = Users.userID WHERE GameID =2 ORDER BY score DESC LIMIT 10'); 
+
+        if ( $dbs->execute() && $dbs->rowCount() > 0 ) {
+            $scores = $dbs->fetchAll(PDO::FETCH_ASSOC); 
+			?><ul> Top 10 Scores:</br><?php
+			foreach ($scores as $value) {
+                            $i++;
+                            echo $i;
+			?> Score: <?php echo '<td>', $value['score'],'</td>';?> 
+			User Name: <?php echo '<td>', $value['userName'],'</td>';?></br><?php
+			echo '</tr>';
+				}
+			}                        
+    }
+    
+    
+    function topTenScoresGame3(){
+        // php code for retrieving top 10 all time scores from game 3 from the Scores table
+        $userID = filter_input(INPUT_POST,'userID');
+        $gameID = filter_input(INPUT_POST,'gameID');
+        $score = filter_input(INPUT_POST,'score');
+	$error_message = '';    
+        $i = 0;
+        $db = new PDO("mysql:host=localhost;dbname=capstonegames", "root", "");
+        $dbs = $db->prepare('SELECT Scores.userID, Scores.gameID, Scores.score, Users.userName FROM Scores INNER JOIN Users ON Scores.userID = Users.userID WHERE GameID =3 ORDER BY score DESC LIMIT 10'); 
+
+        if ( $dbs->execute() && $dbs->rowCount() > 0 ) {
+            $scores = $dbs->fetchAll(PDO::FETCH_ASSOC); 
+			?><ul> Top 10 Scores:</br><?php
+			foreach ($scores as $value) {
+                            $i++;
+                            echo $i;
+			?> Score: <?php echo '<td>', $value['score'],'</td>';?> 
+			User Name: <?php echo '<td>', $value['userName'],'</td>';?></br><?php
+			echo '</tr>';
+				}
+			}                        
+    }
+    
+   
+  
+    function topTenScoresGameforDay(){
+        // php code for retrieving top 10 scores for the day from the Scores table
+        $userID = filter_input(INPUT_POST,'userID');
+        $gameID = filter_input(INPUT_POST,'gameID');
+        $score = filter_input(INPUT_POST,'score');
+	$error_message = '';    
+        $i = 0;
+        $db = new PDO("mysql:host=localhost;dbname=capstonegames", "root", "");
+        $dbs = $db->prepare('SELECT Scores.userID, Scores.gameID, Scores.score, Users.username, Scores.date FROM Scores INNER JOIN Users ON Scores.userID = Users.userID WHERE GameID =1 AND datediff(curdate(), date) <= 1 ORDER BY score DESC LIMIT 10'); 
+
+        if ( $dbs->execute() && $dbs->rowCount() > 0 ) {
+            $scores = $dbs->fetchAll(PDO::FETCH_ASSOC); 
+			?><ul> Top 10 Scores:</br><?php
+			foreach ($scores as $value) {
+                            $i++;
+                            echo $i;
+			?> Score: <?php echo '<td>', $value['score'],'</td>';?> 
+			User Name: <?php echo '<td>', $value['userName'],'</td>';?></br><?php
+			echo '</tr>';
+				}
+			}                        
+    }
                         
-                        
-                        
-                        
-        ?>  
+       
+    function topTenScoresGameforWeek(){
+        // php code for retrieving top 10 scores for the week from the Scores table
+        $userID = filter_input(INPUT_POST,'userID');
+        $gameID = filter_input(INPUT_POST,'gameID');
+        $score = filter_input(INPUT_POST,'score');
+	$error_message = '';    
+        $i = 0;
+        $db = new PDO("mysql:host=localhost;dbname=capstonegames", "root", "");
+        $dbs = $db->prepare('SELECT Scores.userID, Scores.gameID, Scores.score, Users.username, Scores.date FROM Scores INNER JOIN Users ON Scores.userID = Users.userID WHERE GameID =1 AND datediff(curdate(), date) <= 7 ORDER BY score DESC LIMIT 10'); 
+
+        if ( $dbs->execute() && $dbs->rowCount() > 0 ) {
+            $scores = $dbs->fetchAll(PDO::FETCH_ASSOC); 
+			?><ul> Top 10 Scores:</br><?php
+			foreach ($scores as $value) {
+                            $i++;
+                            echo $i;
+			?> Score: <?php echo '<td>', $value['score'],'</td>';?> 
+			User Name: <?php echo '<td>', $value['userName'],'</td>';?></br><?php
+			echo '</tr>';
+				}
+			}                        
+    }
+        
+    
+    function topTenScoresGameforMonth(){
+        // php code for retrieving top 10 scores for the month from the Scores table
+        $userID = filter_input(INPUT_POST,'userID');
+        $gameID = filter_input(INPUT_POST,'gameID');
+        $score = filter_input(INPUT_POST,'score');
+        $error_message = '';    
+        $i = 0;
+        $db = new PDO("mysql:host=localhost;dbname=capstonegames", "root", "");
+        $dbs = $db->prepare('SELECT Scores.userID, Scores.gameID, Scores.score, Users.username, Scores.date FROM Scores INNER JOIN Users ON Scores.userID = Users.userID WHERE GameID =1 AND Month(`date`) = Month(curdate()) ORDER BY score DESC LIMIT 10'); 
+
+        if ( $dbs->execute() && $dbs->rowCount() > 0 ) {
+            $scores = $dbs->fetchAll(PDO::FETCH_ASSOC); 
+			?><ul> Top 10 Scores:</br><?php
+			foreach ($scores as $value) {
+                            $i++;
+                            echo $i;
+			?> Score: <?php echo '<td>', $value['score'],'</td>';?> 
+			User Name: <?php echo '<td>', $value['userName'],'</td>';?></br><?php
+			echo '</tr>';
+				}
+			}                        
+    }
+    
+    function topTenScoresforUser(){
+        // php code for retrieving top 10 scores for a user for a specific game from the Scores table
+        $userID = filter_input(INPUT_POST,'userID');
+        $gameID = filter_input(INPUT_POST,'gameID');
+        $score = filter_input(INPUT_POST,'score');
+			$error_message = '';    
+        $i = 0;
+        $db = new PDO("mysql:host=localhost;dbname=capstonegames", "root", "");
+        $dbs = $db->prepare('SELECT Scores.userID, Scores.gameID, Scores.score, Users.username, Scores.date FROM Scores INNER JOIN Users ON Scores.userID = Users.userID WHERE gameID =1 AND Scores.userID=1 AND datediff(curdate(), date) <= 1 ORDER BY score DESC LIMIT 10'); 
+
+        if ( $dbs->execute() && $dbs->rowCount() > 0 ) {
+            $scores = $dbs->fetchAll(PDO::FETCH_ASSOC); 
+			?><ul> Top 10 Scores:</br><?php
+			foreach ($scores as $value) {
+                            $i++;
+                            echo $i;
+			?> Score: <?php echo '<td>', $value['score'],'</td>';?> 
+			User Name: <?php echo '<td>', $value['userName'],'</td>';?></br><?php
+			echo '</tr>';
+				}
+			}                        
+    }
+?>  
