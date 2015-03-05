635f032d59d1e1e4d854ca439029e3e8ce7ff10b
Mark Falter <mafalter@email.neit.edu>
2015-03-04 21:38:22 -0500

    Updated so footer links reflect main menu links

--- mark/footer.php
+++ mark/footer.php
@@ -1,5 +1,18 @@
 <!-- Start Footer -->
 <div id="footer">
 <p class="lefty">&copy; Copyright 2015, Capstone Arcade.</p>
-<p class="righty"><a href="index.php">Home</a> | <a href="signup.php">Join</a> | <a href="login.php">Login</a></p>
+<p class="righty"><a href="index.php">Home</a> | 
+<?php
+			if (isset($_SESSION['loggedin'])&& $_SESSION['loggedin'] == true) {
+			$_SESSION['userID'] = getUserID($_SESSION['email']);
+			//echo session_id();
+			//var_dump($_SESSION['loggedin']);
+			//var_dump($_SESSION['userID']);
+			echo '<a href="profile.php">Profile</a>'; ?> | <?php
+            echo '<a href="logout.php">Logout</a>';
+			} else {
+			echo '<a href="signup.php">Join</a>'; ?> | <?php
+			echo '<a href="login.php">Login</a>';
+			}
+			?></p>
 </div><!-- End Footer -->
