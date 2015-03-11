14c887ade7811078a115ced10d35b0ba0583cebf
Mark Falter <mafalter@email.neit.edu>
2015-03-11 17:40:57 -0400

    Restructured the main page using the css and new functions.  Also added links to sort the data which don't fully work yet.

--- mark/index.php
+++ mark/index.php
@@ -1,50 +1,73 @@
 <?php include ('header.php');?>
 <!-- Start Inner Wrapper for site content -->
 <!-- Start Inner Wrapper for site content -->
-
 <div id="container">
-<h1 class="title">Capstone Arcade Presents:</h1>
+<h1 class="titleMain">Capstone Arcade Presents:</h1>
 
 <!-- Main content div -->
 <div id="main-content">
 
 <div id="article-wrapper">
-    <h1 class="title2">Breakout</h1>
-    <p>Smash a wall of bricks by deflecting a bouncing ball with a paddle. Score points with each broken brick.  When all the bricks have been destroyed, you’ll advances to a new level. Miss the ball and you’ll lose a life.  Once all lives are lost, the game will end.</p>
+    <h1 class="titleArticle">Breakout</h1>
+    <p class= "descriptionArticle"><?php getGameDescriptionByGameID(1)?></p>
     <img src="../media/Breaker.jpg" alt=""/>
-        <div id="scores">
+	<div id ="scores-wrapper">
+        <div id="mainScores" class="scoresArticle">
 		<?php
+		$sortOption = '';
+		$sortOption = $_GET['sortOption'];
 		// Retrieve top 10 scores
-         topTenScoresGame1();			
+		//if ($sortOption = 'month'){
+		//	topTenScoresGameforMonthAll(1);
+		//	}
+		//else if ($sortOption = 'week'){
+		//	topTenScoresGameforWeekAll(1);
+		//	}
+		//else if ($sortOption = 'day'){
+		//	topTenScoresGameforDayAll(1);
+		//	}
+		//else {
+			topTenScoresGame1();
+		//	}
         ?>
-			<a class="play" href="../games/breakout/index.html"> </a>
+		</div>
+			<p class="sortLinks"><a>Sort By: </a>
+				<a href ="index.php?sortOption=day" method = "get">Day </a> |
+				<a href ="index.php?sortOption=week"> Week </a> |
+				<a href ="index.php?sortOption=month"> Month </a> |
+				<a href ="index.php?sortOption=allTime"> All Time</a>
+				<?php //var_dump($sortOption);?> 
+			</p>
         </div>
+		<a class="play" href="../games/breakout/index.html"> </a>
+		
+		
 </div><!-- end article -->
 
 <div id="article-wrapper">
-    <h1 class="title2">Helicopter</h1>
-    <p>Fly your helicopter across the landscape while trying to avoid obstacles.  The longer you stay in the air, the more points you earn.  The game continues until you hit one of the various obstacles.</p>
+    <h1 class="titleArticle">Helicopter</h1>
+    <p class= "descriptionArticle"><?php getGameDescriptionByGameID(3)?></p>
     <img src="../media/Helicopter.jpg" alt=""/>
-        <div id="scores">
+        <div id="mainScores" class="scoresArticle">
             <?php
             // Retrieve top 10 scores
 			topTenScoresGame3();
             ?>
-			<a class="play" href="../games/helicopter/index.html"> </a>
         </div>
+		<a class="play" href="../games/helicopter/index.html"> </a>
 </div><!-- end article -->
 
 <div id="article-wrapper">
-    <h1 class="title2">Space Invaders</h1>
-    <p>Control a laser cannon by moving it across the bottom of the screen and firing at descending aliens. Defeat aliens as they advance towards you. Earns points by shooting them with the laser cannon. As more aliens are defeated, the aliens' movement speeds up.  If they reach the bottom, the alien invasion is successful and the game ends. </p>
+    <h1 class="titleArticle">Space Invaders</h1>
+    <p class= "descriptionArticle"><?php getGameDescriptionByGameID(2)?></p>
     <img src="../media/Invaders.jpeg" alt=""/>
-        <div id="scores">
+        <div id="mainScores" class="scoresArticle">
              <?php
             // Retrieve top 10 scores
 			topTenScoresGame2();
             ?>
+			</div>
 			<a class="play" href="../games/spaceinvaders/index.html"> </a>
-        </div>
 </div><!-- end article -->
 
 </div><!-- end main content -->
@@ -61,7 +84,9 @@
 	<div class="thumb"><img src="../media/ad.jpg" alt=""/></div>
 	<div class="thumb"><img src="../media/ad.jpg" alt=""/></div>
 	<div class="thumb"><img src="../media/ad.jpg" alt=""/></div>
-
+	<div class="thumb"><img src="../media/ad.jpg" alt=""/></div>
+	<div class="thumb"><img src="../media/ad.jpg" alt=""/></div>
+	
 </div><!--  end sidebar -->
 </div><!-- end inner container -->
 <?php include ('footer.php');?>
