9c4258fd37aef089bcfe1b5ae2862cfd2702f34d
snwelton <snwelton@email.neit.edu>
2015-03-13 20:33:52 -0400

    fixed links

--- games/helicopter/index.php
+++ games/helicopter/index.php
@@ -3,6 +3,10 @@
 <title>Helicopter</title>
 </head>
 <body>
+    <?php
+        $path = $_SERVER['DOCUMENT_ROOT'].'/capstoneproject';
+        include ($path.'/pages/header.php');
+    ?>
 <script src="helicopter.js"></script>
 </body>
 </html>
--- games/spaceinvaders/index.php
+++ games/spaceinvaders/index.php
@@ -3,6 +3,10 @@
 <title>Space Invaders</title>
 </head>
 <body>
+    <?php
+        $path = $_SERVER['DOCUMENT_ROOT'].'/capstoneproject';
+        include ($path.'/pages/header.php');
+    ?>
 <script src="spaceinvaders.js"></script>
 </body>
 </html>
