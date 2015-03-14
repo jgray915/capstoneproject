debe687fe609f8ac8c9787a6f379509300f5a2f4
snwelton <snwelton@email.neit.edu>
2015-03-13 20:04:25 -0400

    fixed page links

--- pages/header.php
+++ pages/header.php
@@ -7,7 +7,7 @@ include ($path.'/validations/functions.php')?>
 <head>
     <meta charset="UTF-8">
     <title>Capstone Arcade</title>
-    <link rel="stylesheet" type="text/css" href="<?php echo '../css/main.css' ?>"/>
+    <link rel="stylesheet" type="text/css" href="<?php echo '/capstoneproject/css/main.css' ?>"/>
 </head>
 
 <body>
@@ -24,15 +24,15 @@ include ($path.'/validations/functions.php')?>
 			session_start();
 			} 
 			?>
-            <a class="btn" href="../pages/home.php">HOME</a>
+            <a class="btn" href="/capstoneproject/pages/home.php">HOME</a>
 			<?php
 			if (isset($_SESSION['loggedin'])&& $_SESSION['loggedin'] == true) {
 			$_SESSION['userID'] = getUserID($_SESSION['email']);
-			echo '<a class="btn" href="../pages/profile.php">PROFILE</a>';
-            echo '<a class="btn" href="../pages/logout.php">LOGOUT</a>';
+			echo '<a class="btn" href="/capstoneproject/pages/profile.php">PROFILE</a>';
+            echo '<a class="btn" href="/capstoneproject/pages/logout.php">LOGOUT</a>';
 			} else {
-			echo '<a class="btn" href="../pages/signup.php">JOIN</a>';
-			echo '<a class="btn" href="../pages/login.php">LOGIN</a>';
+			echo '<a class="btn" href="/capstoneproject/pages/signup.php">JOIN</a>';
+			echo '<a class="btn" href="/capstoneproject/pages/login.php">LOGIN</a>';
 			}
 			?>
         </div>
